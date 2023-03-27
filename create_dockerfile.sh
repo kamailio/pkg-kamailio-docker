41fc01f3197e3fe1539ee0286882b8c27f8a01fb
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-03-27 08:24:40 +0200

    Makefile: stretch moved to archive.debian.org
    https://lists.debian.org/debian-devel-announce/2023/03/msg00006.html
    (cherry picked from commit 6936ff8be0290ad2f0e77b16de2fa25d897efd90)

--- create_dockerfile.sh
+++ create_dockerfile.sh
@@ -64,13 +64,13 @@ case ${dist} in
 	stretch)        CLANG=" clang-3.8" ;;
 	buster)         CLANG=" clang-7" ;;
 	bullseye)       CLANG=" clang-11" ;;
-	sid)            CLANG=" clang-11" ;;
+  *)              CLANG=" clang" ;;
 esac
 
 case ${dist} in
-  focal|bionic|xenial|trusty|precise) base=ubuntu ;;
-  squeeze|wheezy|jessie) base=debian/eol ;;
-  stretch|buster|bullseye|sid) base=debian ;;
+  jammy|focal|bionic|xenial|trusty|precise) base=ubuntu ;;
+  squeeze|wheezy|jessie|stretch) base=debian/eol ;;
+  buster|bullseye|bookworm|sid) base=debian ;;
   *)
     echo "ERROR: no ${dist} base found"
     exit 1
